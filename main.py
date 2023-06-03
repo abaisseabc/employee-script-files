69a299b8c8dd7a322e667caf04c130c0e471d58e
abaisseabc <abaisseabc666@gmail.com>
2023-06-03 22:27:24 +0300

    произведен рефакторинг главной функции

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from get_data_from_api import DataApi
 from create_files import CreateFiles
 
-if __name__ == '__main__':
+
+def main():
     todos = DataApi().get_todos()
     users = DataApi().get_users()
 
     if todos and users:
         CreateFiles(todos, users).create()
     else:
-        print('Данные не были получены')
+        print('Данные о пользователях и задачах не были получены')
+
+
+if __name__ == '__main__':
+    main()
