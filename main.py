1d4f1877ea71f6788c5da2ef006384b2e9d9e46d
abaisseabc <abaisseabc666@gmail.com>
2023-06-10 13:42:22 +0300

    перенесена функция создания директории "tasks"

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from get_data_from_api import DataApi
 from create_files import CreateFiles
+from os import makedirs
+
+OUT_DIR = 'tasks'
+
+
+def create_directory_tasks():
+    try:
+        makedirs(OUT_DIR, exist_ok=True)
+    except OSError:
+        raise OSError(f"Не удается создать каталог назначения {OUT_DIR}!")
 
 
 def main():
+    create_directory_tasks()
+
     todos = DataApi().get_todos()
     users = DataApi().get_users()
 
     if todos and users:
-        CreateFiles(todos, users).create()
+        CreateFiles(todos, users, OUT_DIR).create()
     else:
         print('Данные о пользователях и задачах не были получены')
 
